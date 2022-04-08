0937f97b769fccc648127c3a743dbfede0de04c6
V Vijay Ananth <divingvj@gmail.com>
2022-04-08 13:24:29 +0800

    Iniial commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,15 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
+
 /* File placeholder */
+char    *get_next_line(int fd)
+{
+    static char    buff[20];
+    size_t  num;
+
+    num = sizeof(buff);
+    read(fd, buff, num);
+
+    return (buff);
+}
