839458d5e6390e18a16aa6eb40450b1842eb38fe
V Vijay Ananth <divingvj@gmail.com>
2022-05-23 00:09:06 +0800

    Homework

--- get_next_line.c
+++ get_next_line.c
@@ -1,19 +1,35 @@
 #include "get_next_line.h"
 #include <stdio.h>
+#include <string.h>
 
-/* File placeholder */
-char    *get_next_line(int fd)
+char	*fill_buffer(int fd)
 {
-    static char    buff[BUFFER_SIZE];
-    int		len;
-	size_t  num;
+	int		textlen;
 
-    num = sizeof(buff);
-    len = read(fd, buff, num);
-	if (len == -1)
+	printf("Before Textlen %d Buff %s\n", textlen, buff);
+	textlen = read(fd, buff, BUFFER_SIZE);
+	printf("After Textlen %d Buff %s\n", textlen, buff);
+	bzero(buff, space);
+	return (buff);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		buff[BUFFER_SIZE];
+	int				textlen;
+	int				space;
+
+	space = BUFFER_SIZE;
+	textlen = 1;
+	printf("Text len, space %d %d\n", textlen, space);
+	// approach - fill buffer output buffer, 
+	// empty buffer fill buffer output buffer
+	while (textlen > 0)
 	{
-		printf("Error");
-		return (0);
+		printf("Before Textlen %d Buff %s\n", textlen, buff);
+		textlen = read(fd, buff, BUFFER_SIZE);
+		printf("After Textlen %d Buff %s\n", textlen, buff);
+		bzero(buff, space);
 	}
-    return (buff);
+	return (buff);
 }
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "get_next_line.h"
 #include <stdio.h>
+#include <string.h>
 
 int	main (void)
 {
@@ -7,6 +8,8 @@ int	main (void)
 	char	*str;
 	fd = open("text.txt", O_RDONLY);
 	str = get_next_line(fd);
-	printf("String is %s", str);
+	printf("String is %s\n", str);
+//	str = get_next_line(fd);
+//	printf("String is %s\n", str);
 	return (0);
 }
