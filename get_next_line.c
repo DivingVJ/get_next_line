97b9f07f08a2b21529498e0f11cd381f6fdfa288
V vijay ananth Velayutham <vvelayut@u91z03s01.42kl.edu.my>
2022-05-23 17:42:23 +0800

    23/5 work

--- get_next_line.c
+++ get_next_line.c
@@ -2,7 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
-char	*fill_buffer(int fd)
+char	check_buffer(char *buff)
+{
+	int	i;
+
+	i = 0;
+	if (buff[i] == '\n')
+		return ('N');
+	else if (buff[i] == '\0')
+		return ('E');
+	while(i < BUFFER_SIZE)
+	{
+		if (buff[i] == '\n')
+			return ('L');
+		i++;
+	}
+	return ('B');
+}
+
+void	save_buffer(char *buff)
+{
+	printf("string %s", buff);
+}
+
+/*char	*fill_buffer(int fd)
 {
 	int		textlen;
 
@@ -11,25 +34,30 @@ char	*fill_buffer(int fd)
 	printf("After Textlen %d Buff %s\n", textlen, buff);
 	bzero(buff, space);
 	return (buff);
-}
+} */
 
 char	*get_next_line(int fd)
 {
 	static char		buff[BUFFER_SIZE];
 	int				textlen;
 	int				space;
+	char			flag;
 
 	space = BUFFER_SIZE;
-	textlen = 1;
-	printf("Text len, space %d %d\n", textlen, space);
-	// approach - fill buffer output buffer, 
-	// empty buffer fill buffer output buffer
-	while (textlen > 0)
+	bzero(buff, space);
+	textlen = read(fd, buff, BUFFER_SIZE);
+	flag = check_buffer(buff);
+	printf("Flag = %c, Textlen = %d\n", flag, textlen);
+	if (flag == 'B') //text > buffer size
+		save_buffer(buff);
+	else if (flag == 'E') //EOF reached
+		return (NULL);	
+	else if (flag == 'N')
 	{
-		printf("Before Textlen %d Buff %s\n", textlen, buff);
-		textlen = read(fd, buff, BUFFER_SIZE);
-		printf("After Textlen %d Buff %s\n", textlen, buff);
-		bzero(buff, space);
+		bzero(buff,space);
+		memset(buff, '\n', 1);
 	}
+//	else if (flag == 'L')
+
 	return (buff);
 }
--- main.c
+++ main.c
@@ -8,8 +8,10 @@ int	main (void)
 	char	*str;
 	fd = open("text.txt", O_RDONLY);
 	str = get_next_line(fd);
-	printf("String is %s\n", str);
-//	str = get_next_line(fd);
-//	printf("String is %s\n", str);
+	printf("String1 is %s", str);
+	str = get_next_line(fd);
+	printf("String2 is %s", str);
+	str = get_next_line(fd);
+	printf("String3 is %s", str);
 	return (0);
 }
