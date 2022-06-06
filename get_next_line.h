dfb8c920c1d2f752688810d0589a07921ad5b338
V vijay ananth Velayutham <vvelayut@u91z03s03.42kl.edu.my>
2022-06-06 15:38:36 +0800

    Headers added

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vvelayut <vvelayut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 15:38:01 by vvelayut          #+#    #+#             */
+/*   Updated: 2022/06/06 15:38:09 by vvelayut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -12,8 +24,6 @@ char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strdup(const char *src);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
